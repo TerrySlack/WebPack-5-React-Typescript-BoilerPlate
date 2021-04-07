2b16b5576a29c8c7e5eafd71ad933a6205345179
Terry Slack <tslack@mainframework.com>
2021-04-07 11:32:55 -0700

    updating prettier, and eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,18 @@
-import React from 'react'
+import React from 'react';
 // import { Provider } from "react-redux";
 // import ApolloRootProvider from "HOC/Apollo";
 // import { ThemeProvider } from "react-jss";
 // import createStore from "../store";
 // import Routes from "../routes";
-import Test from 'Components/Test'
-import './App.css'
+import Test from 'Components/Test';
+import './App.css';
 //const store = createStore();
 const App = () => {
   return (
     <div>
       <Test />
     </div>
-  )
-  //   return (
-  //     <ThemeProvider theme={theme}>
-  //       <Provider store={store}>
-  //         <ApolloRootProvider>
-  //           <Routes />
-  //         </ApolloRootProvider>
-  //       </Provider>
-  //     </ThemeProvider>
-  //   );
-}
+  );
+};
 
-export default App
+export default App;
