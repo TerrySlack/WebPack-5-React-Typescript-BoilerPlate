94ee0ef4ef13a924871c56391631e8d979c323cf
Terry Slack <tslack@mainframework.com>
2021-12-23 14:28:17 -0800

    remvoed Redux

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { Provider } from "react-redux";
 
 import "./App.scss";
-import createStore from "../store";
 import "./App.css";
 import { AppRoutes } from "../routes";
 
-// Create the store
-const store = createStore();
-
 // Render the app
 const App = function () {
-  return (
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
--- src/store/combinedReducers.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-
-// Import Reducers
-import { HomeReducer } from "Containers/Home";
-// Combine them
-export default combineReducers({
-  home: HomeReducer,
-});
