24ea900bc6a69505d613647052a23ba1b1b95b33
Terry Slack <tslack@mainframework.com>
2022-06-23 19:55:57 -0700

    updating package.json

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import "./App.scss";
 import "./App.css";
 import { AppRoutes } from "../routes";
 
--- src/shared/utils/sortData.ts
+++ src/shared/utils/sortData.ts
@@ -1,6 +1,10 @@
-import orderBy from 'lodash/orderBy';
+import orderBy from "lodash/orderBy";
 
 // Sort the arr (array) by the columnName and direction, returning a newly sorted array
-const customSort = (columnName: string, direction: string, arr: any) => orderBy(arr, [columnName], [direction]);
+const customSort = (
+  columnName: string,
+  direction: boolean | "desc" | "asc",
+  arr: any
+) => orderBy(arr, [columnName], [direction]);
 
 export default customSort;
